64131ded6b5f1f710db4c438603b24a1902f6574
Nelson Rodrigues <nelson@waveweb2.com>
2014-03-04 19:05:42 +0000

    First commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < ApplicationController
+  def home
+    render 'users/home'
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todos::Application.config.secret_token = 'ed622fb16076293f7bb275cd5b7a8d513c69acbe386e9a7abcf6d5a4c68cae2f090433352a649b2102605ba59b1c8fc1f680efe0826f2d88d8219bd7686bfdd8'
