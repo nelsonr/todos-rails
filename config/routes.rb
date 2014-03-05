de806a4eb599e6500695d0e1d5dc346fb5b0ede0
Nelson Rodrigues <nelson@waveweb2.com>
2014-03-05 09:51:01 +0000

    Added custom routes for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Todos::Application.routes.draw do
-  devise_for :users
+  devise_for :users,
+             :path => '',
+             :path_names => { sign_in:  "login",
+                              sign_out: "logout",
+                              sign_up:  "register" }
+
+  root :to => 'todos#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
