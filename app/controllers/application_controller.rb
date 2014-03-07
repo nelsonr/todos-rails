386a5ea28b1fa7a4337f4d9d744de2e69a100c0d
Nelson Rodrigues <nelson@waveweb2.com>
2014-03-07 10:16:47 +0000

    Fixed the column sort, god damn it!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,15 +6,17 @@ class ApplicationController < ActionController::Base
       todos = todos_search(todos)
     end
 
+    todos = todos.joins(:user).order("#{sort_column} #{sort_order}")
+
     render_json(todos, total_todos)
   end
 
   def todos_search(todos)
-    todos.joins(:user).where('lower(todos.title) LIKE :search OR lower(users.name) LIKE :search', search: "%#{params[:sSearch]}%".downcase)
+    todos.where('LOWER(todos.title) LIKE :search OR LOWER(users.name) LIKE :search', search: "%#{params[:sSearch]}%".downcase)
   end
 
   def render_json(todos, total_todos)
-    todos_json = Hash.new
+    todos_json = {}
     todos_json[:sEcho] = params[:sEcho].to_i
     todos_json[:iTotalRecords] = todos.count
     todos_json[:iTotalDisplayRecords] = total_todos
@@ -24,5 +26,14 @@ class ApplicationController < ActionController::Base
     end
 
     todos_json
+  end
+
+  def sort_column
+    columns = %w[todos.title users.name todos.created_at]
+    columns[params[:iSortCol_0].to_i]
+  end
+
+  def sort_order
+    params[:sSortDir_0] == "desc" ? "DESC" : "ASC"
   end
 end
